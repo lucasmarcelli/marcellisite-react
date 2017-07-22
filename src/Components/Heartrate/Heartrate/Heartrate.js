58ccc4a3af57f212daded79804532b9e092fce50
Lucas Marcelli <lucas@marcelli.ca>
2017-07-22 11:57:56 -0400

    Switchin computers

---
+++ src/Components/Heartrate/Heartrate/Heartrate.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+
+class Heartrate extends Component {
+
+  render(){
+    return null;
+  }
+}
